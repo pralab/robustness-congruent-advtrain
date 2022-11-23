a40d1d1de7ff5980de5c391901a0656a1964462a
Daniele Angioni <angioni.daniele97.00@gmail.com>
2022-11-23 10:47:37 +0100

    sample weight modified in svm

--- android_churn.py
+++ android_churn.py
@@ -1,39 +1,51 @@
 from sklearn.svm import LinearSVC
 import numpy as np
 import os
-from utils.load_android_features import DS_PATH
+from utils.load_android_features import DS_PATH, generate_random_temporal_features
 import pickle
 from tesseract import temporal
 from utils.eval import compute_nflips, compute_pflips
-from scipy.sparse import vstack
+from scipy.sparse import vstack, csr_matrix
 from sklearn.metrics import precision_recall_fscore_support, precision_score, recall_score, f1_score
 
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-    with open(os.path.join(DS_PATH, 'drebin_xyt.pkl'), 'rb') as f:
-        ds = pickle.load(f)
-    X, y, t, m = ds['X'], ds['y'], ds['t'], ds['m']
-    y = np.array([int(y[0]) for y in y])
-    # Partition dataset
-    splits = temporal.time_aware_train_test_split(
-       X, y, t, train_size=12, test_size=1, granularity='month')
-    X_train, X_tests, y_train, y_tests, t_train, t_tests, train, tests = splits
-    # y_train = [int(y[0]) for y in y_train]
+    # with open(os.path.join(DS_PATH, 'drebin_xyt.pkl'), 'rb') as f:
+    #     ds = pickle.load(f)
+    # X, y, t, m = ds['X'], ds['y'], ds['t'], ds['m']
+    # y = np.array([int(y[0]) for y in y])
+    # # Partition dataset
+    # splits = temporal.time_aware_train_test_split(
+    #    X, y, t, train_size=12, test_size=1, granularity='month')
+    # X_train, X_tests, y_train, y_tests, t_train, t_tests, train, tests = splits
+    # # y_train = [int(y[0]) for y in y_train]
+
+    X, y = generate_random_temporal_features(n_samples_per_month=100,
+                                             n_features=100,
+                                             n_months=6)
+    X_train_i = X[0]
+    y_train_i = y[0]
+    X_tests, y_tests = X[1:], y[1:]
+
+    # X_train_i = X_train
+    # y_train_i = y_train
 
     precs, recs, f1s = [], [], []
     nfrs_pos, pfrs_pos = [], []
     nfrs_neg, pfrs_neg = [], []
 
-    X_train_i = X_train
-    y_train_i = y_train
-
+    sample_weights = None
     n_updates = len(X_tests) - 3
     for i in range(n_updates):
         print(f"> M{i} ({i}/{n_updates})")
+
         clf = LinearSVC()
-        clf.fit(X_train_i, y_train_i)
+        clf.fit(X_train_i, y_train_i, sample_weight=sample_weights)
+
+        preds_tr = clf.predict(X_train_i)
+        sample_weights = ((preds_tr == y_train_i)*2).astype(np.float_)
 
         X_test_i = vstack((X_tests[i], X_tests[i+1]))
         y_test_i = np.hstack((y_tests[i], y_tests[i+1]))
@@ -89,7 +101,7 @@ if __name__ == "__main__":
         ax[i].legend()
     fig.tight_layout()
     fig.show()
-    fig.savefig(os.path.join('images/android', 'android_churn.pdf'))
+    #fig.savefig(os.path.join('images/android', 'android_churn.pdf'))
     print("")
 
 
