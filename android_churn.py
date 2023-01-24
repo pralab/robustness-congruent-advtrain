bf0003c2158ad4c92235e31bbed0671e2f833c95
DanieleAngioni <angioni.daniele97.00@gmail.com>
2023-01-24 11:01:31 +0100

    pipeline for svm

--- android_churn.py
+++ android_churn.py
@@ -3,103 +3,104 @@ from utils.visualization import plot_sequence_results_android
 from itertools import product as cartesian_product
 from utils.android_trainer import AndroidTemporalTrainer
 
-from collections import OrderedDict
-from functools import partial
-from multiprocessing import Pool
-
-def grid_search(root, global_param_dict):
-    ordered_params = OrderedDict(global_param_dict)
-    all_cross_validation_parameters = cartesian_product(*ordered_params.values())
-    names = list(ordered_params.keys())
-
-    partial_f = partial(train_single, root=root)
-    with Pool(processes=1) as p:
-        p.map(partial_f, all_cross_validation_parameters)
-
-
-def train_single(clf_name, root, test_size=5, train_size=12, val_size=0, max_fpr=0.01,
-                 n_updates=12, sample_weight=None, class_weight=None,
-                 temporal_weight=False, overwrite=False,
-                 n_estimators=10, max_depth=5,
-                 C=0.01, max_iter=1000):
-
-
-    fname = f"cw-{class_weight}_" \
-            f"tr-{train_size}_" \
-            f"ts-{test_size}_" \
-            f"val-{val_size}_" \
-            f"fpr-{max_fpr}_" \
-            f"n_updates-{n_updates}_" \
-            f"{clf_name}"
-    if clf_name == 'rf':
-        fname += f"_n_estim-{n_estimators}_" \
-                 f"max_depth-{max_depth}"
-        clf_info = {'clf_name': clf_name,
-                    'n_estimators': n_estimators,
-                    'max_depth': max_depth}
-    elif clf_name == 'svm':
-        fname += f"_C-{C}_" \
-                 f"max_iter-{max_iter}"
-        clf_info = {'clf_name': clf_name,
-                    'C': C,
-                    'max_iter': max_iter}
-
-    print(clf_info)
-    # results_path = os.path.join(root, f"{fname}.pkl")
-    # if not os.path.isdir(root):
-    #     os.makedirs(root)
-    #
-    # fig_fname = fname
-    #
-    # title = f"{clf_info}" \
-    #         f"CW: {class_weight}, " \
-    #         f"TR: {train_size}, " \
-    #         f"TS: {test_size}, " \
-    #         f"VAL: {val_size}, " \
-    #         f"FPR: {max_fpr}, " \
-    #         f"Updates: {n_updates}"
-    #
-    # if not os.path.exists(results_path) or overwrite:
-    #     trainer = AndroidTemporalTrainer(results_path=results_path,
-    #                                      train_size=train_size,
-    #                                      test_size=test_size,
-    #                                      val_size=val_size,
-    #                                      max_fpr=max_fpr,
-    #                                      n_updates=n_updates,
-    #                                      clf_info=clf_info,
-    #                                      class_weight=class_weight,
-    #                                      sample_weight=sample_weight,
-    #                                      temporal_weight=temporal_weight,
-    #                                      overwrite=overwrite)
-    #     trainer.train_sequence_parametric()
-
-    # plot_sequence_results_android(results_path=results_path,
-    #                               fig_fname=fig_fname,
-    #                               title=title)
 
 def main_train_sequence():
 
-    root = "results/android/prova"
+    # clf_info = {'clf_name': 'svm',
+    #             'C': 0.01,
+    #             'max_iter': 1000}
 
-    global_param_dict = {
-        'clf_name': ['svm'],
-        'C': [0.01], #[1e-3, 5e-3, 1e-2, 5e-2, 1e-1],
-        'max_iter': [1000],
-        # 'clf_name': 'rf',
-        # 'n_estimators': [10, 50, 100, 1000],
-        # 'max_depth': [10, 20, 50, 100],
-        'overwrite': [False],
-        'test_size': [5],
-        'train_size': [12],
-        'val_size': [0], #[0, 1, 2],
-        'n_updates': [12],
-        # temporal_weight = False
-        'class_weight': ['balanced', None],
-        'sample_weight': [[None, 2, 5, 10, 100]],
-        'max_fpr': [0.01],  #[0.01, 0.02, 0.05]
-    }
+    # clf_info = {'clf_name': 'rf',
+    #             'n_estimators': 100,
+    #             'max_depth': 20}
 
-    grid_search(root, global_param_dict)
+    # n_estimators = [10, 50, 100, 1000]
+    # max_depth = [10, 20, 50, 100]
+    # prod = list(cartesian_product(n_estimators, max_depth))
+    # n_runs = len(prod)
+    # for i, (n_estimators, max_depth) in enumerate(prod):
+    #     print(f"{i+1}/{n_runs}: n_estim: {n_estimators}, max_depth: {max_depth}")
+    #     clf_info = {'clf_name': 'rf',
+    #                 'n_estimators': n_estimators,
+    #                 'max_depth': max_depth}
+
+    C = [1e-3, 5e-3, 1e-2, 5e-2, 1e-1]
+    class_weight = ['balanced', None]
+    val_size = [0, 1, 2]
+    max_fpr = [0.01, 0.02, 0.03]
+    prod = list(cartesian_product(C, class_weight, val_size, max_fpr))
+    n_runs = len(prod)
+    for i, (C, class_weight, val_size, max_fpr) in enumerate(prod):
+        print(f"{i+1}/{n_runs}: C: {C}, class_weight: {class_weight}, val_size: {val_size}, max_fpr: {max_fpr}")
+        clf_info = {'clf_name': 'svm',
+                    'C': C,
+                    'max_iter': 1000}
+
+        # class_weight = 'balanced'
+        # sample_weight_list = [None, 2, 5, 10, 100, 1000]
+        sample_weight = [None, 2, 5, 10, 100]
+        overwrite = False
+        test_size = 5
+        train_size = 12
+        # val_size = 0 #1
+        # max_fpr = 0.01
+        n_updates = 12
+        temporal_weight = False
+
+        # class_weight_list = [None, 'balanced']
+        # max_fpr_list = [.01, .02, .05]
+        # prod = list(cartesian_product(class_weight_list, max_fpr_list))
+        # n_runs = len(prod)
+
+        # for i, (class_weight, max_fpr) in enumerate(prod):
+        #     print(f"{i+1}/{n_runs}: cw: {class_weight}, max_fpr: {max_fpr}")
+
+        fname = f"cw-{class_weight}_" \
+                f"tr-{train_size}_" \
+                f"ts-{test_size}_" \
+                f"val-{val_size}_" \
+                f"fpr-{max_fpr}_" \
+                f"n_updates-{n_updates}_" \
+                f"{clf_info['clf_name']}"
+        if clf_info['clf_name'] == 'rf':
+            fname += f"_n_estim-{clf_info['n_estimators']}_" \
+                     f"max_depth-{clf_info['max_depth']}"
+
+        # fname = "results_cw-balanced_tr-12_ts-3_C-[0.01]"
+
+        root = "results/android/prova"
+        results_path = os.path.join(root, f"{fname}.pkl")
+        if not os.path.isdir(root):
+            os.makedirs(root)
+
+        fig_fname = fname
+
+        title = f"{clf_info}" \
+                f"CW: {class_weight}, " \
+                f"TR: {train_size}, " \
+                f"TS: {test_size}, " \
+                f"VAL: {val_size}, " \
+                f"FPR: {max_fpr}, " \
+                f"Updates: {n_updates}"
+
+        if not os.path.exists(results_path) or overwrite:
+            trainer = AndroidTemporalTrainer(results_path=results_path,
+                                             train_size=train_size,
+                                             test_size=test_size,
+                                             val_size=val_size,
+                                             max_fpr=max_fpr,
+                                             n_updates=n_updates,
+                                             clf_info=clf_info,
+                                             class_weight=class_weight,
+                                             sample_weight=sample_weight,
+                                             temporal_weight=temporal_weight,
+                                             overwrite=overwrite)
+            trainer.train_sequence_parametric()
+
+
+        plot_sequence_results_android(results_path=results_path,
+                                  fig_fname=fig_fname,
+                                  title=title)
 
 
 if __name__ == "__main__":
