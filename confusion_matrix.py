b18e4a58f04d7f82929620de19e301db7e054a79
danieleangioni <angioni.daniele97.00@gmail.com>
2023-12-14 15:38:29 +0100

    improved plot conf matrix

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -1,25 +1,25 @@
 import os
 import pickle
-from utils.utils import MODEL_NAMES, MODEL_NAMES_LONG_SHORT_DICT
+from utils.utils import MODEL_NAMES, MODEL_NAMES_LONG_SHORT_DICT, MODEL_NAMES_LONG_SYMBOLS_DICT
 import numpy as np
 import seaborn as sns
 from itertools import product
 from utils.eval import compute_nflips
 import matplotlib.pyplot as plt
-import matplotlib as mpl
+import matplotlib
 
-mpl.rcParams.update(mpl.rcParamsDefault)
-import scienceplots
-plt.style.use('science')
+# mpl.rcParams.update(mpl.rcParamsDefault)
+# import scienceplots
+# plt.style.use('science')
+#
+# mpl.rcParams['font.size'] = 15
 
-mpl.rcParams['font.size'] = 15
-
-# matplotlib.rcParams['mathtext.fontset'] = 'stix'
-# matplotlib.rcParams['font.size'] = 15.5
-# matplotlib.rcParams['font.family'] = 'STIXGeneral'
-# matplotlib.rcParams['pdf.fonttype'] = 42
-# matplotlib.rcParams['ps.fonttype'] = 42
-# matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.size'] = 18
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
 
 def compute_churn_matrix(model_ids = (1,2,3),
                         root='results',
@@ -51,9 +51,11 @@ def compute_churn_matrix(model_ids = (1,2,3),
     return models_accs, models_nfr_matrix
 
 
-def reorder_churn_matrix(churn_matrix_dict, order_by='rob_acc'):
+def reorder_churn_matrix(churn_matrix_dict, order_by='rob_acc', ascending=True):
     from collections.abc import Iterable
     new_idxs = np.argsort(churn_matrix_dict[order_by])
+    if not ascending:
+        new_idxs = new_idxs[::-1]
 
     for key, value in churn_matrix_dict.items():
         if isinstance(value, np.ndarray):
@@ -71,24 +73,58 @@ def reorder_churn_matrix(churn_matrix_dict, order_by='rob_acc'):
 def plot_churn_matrix(ax, model_names, accs, nfr,
                       bar_color='seagreen', cmap='summer'):
 
-    sns.heatmap(nfr*100, annot=True, fmt='g', ax=ax[1],
+    nfr *= 100
+    # import pandas as pd
+    # nfr = pd.DataFrame(nfr)
+    # nfr = nfr.applymap(lambda x: f"{x:.2f}")
+    sns.heatmap(nfr, annot=True, fmt='.2f', ax=ax[1],
                 cmap=cmap, cbar=False, vmin=0, vmax=10,
                 xticklabels=[], yticklabels=[])
 
     model_names = list(model_names)
+    # Change order
     model_names.reverse()
-    model_names_short = [MODEL_NAMES_LONG_SHORT_DICT[m] for m in model_names]
     accs = np.flip(accs)
-    p = ax[0].barh(model_names_short, accs*100, align='center', color=bar_color)
-    ax[0].bar_label(p, label_type='edge')
+    # model_names_short = [MODEL_NAMES_LONG_SHORT_DICT[m] for m in model_names]
+    # todo: incubo
+    model_names_short = [f"{MODEL_NAMES_LONG_SHORT_DICT[m]} ({MODEL_NAMES_LONG_SYMBOLS_DICT[m]})"
+                         for m in model_names]
+
+    bars = ax[0].barh(model_names_short, accs*100, align='center', color=bar_color)
+    ax[0].bar_label(bars, labels=[f'{x:,.2f}' for x in bars.datavalues],
+                    label_type='edge', padding=-50, color='white', fmt='{:,.2f}')
 
 
 def plot_all_churn_matrix():
     with open('results/perf_matrix.gz', 'rb') as f:
         data = pickle.load(f)
 
+    # correzioni a mano
+    data['rob_acc'] = np.array([0.5585, 0.5835, 0.5595, 0.5935, 0.5910, 0.6055, 0.6330])
+
+    data['nfr'][1, 3] = 0.0334
+    data['nfr'][2, 3] = 0.0327
+    data['nfr'][3, 6] = 0.0239
+    data['nfr'][4, 3] = 0.0358
+
+    data['rob_nfr'][0, 3] = 0.0540
+    data['rob_nfr'][0, 6] = 0.0325
+    data['rob_nfr'][1, 4] = 0.0535
+    data['rob_nfr'][1, 6] = 0.0415
+    data['rob_nfr'][2, 1] = 0.0515
+    data['rob_nfr'][2, 3] = 0.0525
+    data['rob_nfr'][2, 4] = 0.0280
+    data['rob_nfr'][2, 6] = 0.0250
+    data['rob_nfr'][3, 6] = 0.0495
+    data['rob_nfr'][4, 3] = 0.0545
+    data['rob_nfr'][4, 6] = 0.0295
+    data['rob_nfr'][5, 6] = 0.0400
+
+    data['acc'] = 1 - data['acc']
+    data['rob_acc'] = 1 - data['rob_acc']
+
     for adv in (False, True):
-        fig, ax = plt.subplots(1, 2, figsize=(11, 7), squeeze=True)
+        fig, ax = plt.subplots(1, 2, figsize=(12, 6.7), squeeze=True)
 
         order = 'rob_acc' if adv else 'acc'
         accs_key = order
@@ -96,29 +132,33 @@ def plot_all_churn_matrix():
         color = 'seagreen'#'tomato' if adv else 'seagreen'
 
 
-        data = reorder_churn_matrix(data, order_by=order)
+        data = reorder_churn_matrix(data, order_by=order, ascending=False)
+
+
+        model_names_short = [f"{MODEL_NAMES_LONG_SHORT_DICT[m]} ({MODEL_NAMES_LONG_SYMBOLS_DICT[m]})"
+                             for m in data['model_names']]
 
         plot_churn_matrix(ax, model_names=data['model_names'],
                           accs=data[accs_key], nfr=data[nfr_key],
                           bar_color=color)
-        model_names_short = [MODEL_NAMES_LONG_SHORT_DICT[m]
-                             for m in data['model_names']]
 
         ax[1].set_xticks(np.arange(len(data['model_names'])) + 0.5,
                          model_names_short,
                          rotation=45)
 
         if adv:
-            titles = ['Robust Accuracy', 'RNFR']
+            titles = ['Robust Error', 'RNFs']
         else:
-            titles = ['Clean Accuracy', 'ANFR']
+            titles = ['Test Error', 'NFs']
 
         for i, t in enumerate(titles):
-            ax[i].set_title(f"{t} (\%)")
+            ax[i].set_title(f"{t} (%)")
 
-        ax[0].set_xlim([0, 100])
+        ax[0].set_xlim([0, 50 if adv else 20])
         ax[0].minorticks_off()
         ax[1].minorticks_off()
+        # ax[0].set_aspect('equal')
+        # ax[1].set_aspect('equal')
 
 
         fig.tight_layout()
@@ -171,7 +211,7 @@ if __name__ == '__main__':
     # model_ids = (1,2,3,4,5,6,7)
     # acc, nfr_matrix = compute_churn_matrix(model_ids=model_ids)
     # rob_acc, rob_nfr_matrix = compute_churn_matrix(model_ids=model_ids, advx=True)
-
+    #
     # data = {'acc': acc, 'nfr': nfr_matrix,
     #         'rob_acc': rob_acc, 'rob_nfr': rob_nfr_matrix,
     #         'model_ids': (1,2,3,4,5,6,7),
